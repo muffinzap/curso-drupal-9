ff717ca07fda8011e7a5aee6edb7a5cef9f15ad7
Carlos Junquera <carlos@galicloud.com>
2022-10-17 12:39:37 +0200

    galicloud_olivero subtheme

---
+++ web/themes/custom/galicloud_olivero/js/galicloud-olivero.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Galicloud Olivero behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.galicloudOlivero = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
