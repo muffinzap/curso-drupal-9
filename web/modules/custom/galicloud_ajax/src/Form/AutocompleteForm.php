bd52faf631b8b105ae72c230ef0cb75a7632edd7
Carlos Junquera <carlos@galicloud.com>
2022-10-21 09:59:53 +0200

    galicloud_ajax create taxonomy term on save if not exists

--- web/modules/custom/galicloud_ajax/src/Form/AutocompleteForm.php
+++ web/modules/custom/galicloud_ajax/src/Form/AutocompleteForm.php
@@ -28,6 +28,29 @@ class AutocompleteForm extends FormBase {
       '#autocomplete_route_name' => 'galicloud_ajax.autocomplete_user',
     ];
 
+
+    //entity_autocomplete
+    $form['selected_node'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => 'Select a content',
+      '#target_type' => 'node',
+      '#selection_handler' => 'default',
+      '#selection_settings' => [
+        'target_bundles' => ['article', 'page'],
+      ],
+    ];
+
+
+    //entity_autocomplete con autocreate
+    $form['tags'] = [
+      '#title' => 'Category',
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'taxonomy_term',
+      '#autocreate' => [
+        'bundle' => 'tags',
+      ],
+    ];
+
     $form['actions'] = [
       '#type' => 'actions',
     ];
@@ -52,6 +75,21 @@ class AutocompleteForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    //Create taxonomy term on submit
+    $tag = $form_state->getValue('tags');
+    if (empty($tag)) {
+      $this->messenger()->addStatus("Tag is empty, nothing to do");
+    }
+    elseif (is_string($tag)) {
+      $this->messenger()->addStatus("A term selected, tid = $tag");
+    }
+    elseif (isset($tag['entity']) && ($tag['entity'] instanceof \Drupal\taxonomy\Entity\Term)) {
+      $entity = $tag['entity'];
+      $entity->save();
+      $this->messenger()->addStatus("A new term : " . $entity->id() . " : " . $entity->label());
+    }
+
     $this->messenger()->addStatus($this->t('The message has been sent.'));
     $form_state->setRedirect('<front>');
   }
